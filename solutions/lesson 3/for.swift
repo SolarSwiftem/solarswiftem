1bc45ffa851637ef82e72f7d6f15fd3f80e04bcd
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-03-22 17:20:54 +0000

    Added question identifiers in comments.

--- solutions/lesson 3/for.swift
+++ solutions/lesson 3/for.swift
@@ -1,19 +1,24 @@
+// Question 1
 for var i=1; i<11; i++ {
 	print(i)
 }
 
+// Question 2
 for var i=0; i<51; i+=5 {
 	print(i)
 }
 
+// Question 3
 for var i=10; i>0; i-- {
 	print(i)
 }
 
+// Question 4
 for var i=40; i>=0; i-=4 {
 	print(i)
 }
 
+// Question 5
 for var i=1; i^2<=81; i++ {
 	print(i)
 }
--- solutions/lesson 3/while.swift
+++ solutions/lesson 3/while.swift
@@ -1,29 +1,34 @@
+// Question 1
 var i = 1
 while i < 11 {
 	print(i)
 	i++
 }
 
+// Question 2
 var j = 0
 while j <= 50 {
 	print(j)
 	j+=5
 }
 
+// Question 3
 var k = 10
 while k > 0 {
 	print(k)
 	k--
 }
 
+// Question 4
 var l = 40
 while l >= 0 {
 	print(l)
 	l-=4
 }
 
+// Question 5
 var m = 1
 while m^2 <= 81 {
 	print(m)
 	m++
-} 
+}

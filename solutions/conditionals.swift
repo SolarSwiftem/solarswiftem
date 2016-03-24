242a422b01a0906b43ef66a2ca81d8049ce85ce5
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-03-24 10:30:36 +0000

    Moved solutions for conditionals and basic from drive.

---
+++ solutions/conditionals.swift
@@ -0,0 +1,15 @@
+var x = 2
+
+if x < 3    {
+	print("Launch successful")
+}	else	{
+	print("Launch failure")
+}
+
+if x < 3    {
+	print("Launch successful")
+}	else if x < 8	{
+	print("Launch delayd")
+}	else	{
+	print("Launch failure")
+}
---
+++ solutions/lesson 1.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+// My first program in Swift
+var myString = "Hello, Solar Swiftem!"
+
+print(myString)
+
+
+// this is a comment
+
+/* this is also a comment,
+but written over multiple lines */
